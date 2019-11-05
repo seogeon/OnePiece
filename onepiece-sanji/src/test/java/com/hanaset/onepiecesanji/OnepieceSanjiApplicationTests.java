d8748793101b17c03c6407b557c4b6b290b2b5ec
Luke <b183523@gmail.com>
2019-11-05 17:19:55 +0900

    멀티 모듈 생성

---
+++ onepiece-sanji/src/test/java/com/hanaset/onepiecesanji/OnepieceSanjiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanaset.onepiecesanji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnepieceSanjiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
