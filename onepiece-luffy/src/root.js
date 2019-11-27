ac82b7138419f1bfafd7c0284ede580dc7f4e744
Luke <b183523@gmail.com>
2019-11-27 16:41:25 +0900

    오타 수정

--- onepiece-luffy/src/root.js
+++ onepiece-luffy/src/root.js
@@ -1,15 +1,12 @@
 import React from "react";
 import {BrowserRouter} from 'react-router-dom';
 import App from "./App";
-import {CookiesProvider} from 'react-cookie';
 
 
 const Root = () => (
-    <CookiesProvider>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-    </CookiesProvider>
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
 );
 
 export default Root;
